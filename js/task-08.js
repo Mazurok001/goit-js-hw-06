e3838430c491a0578cccc828f078bdf3dd2f040b
Mazurok001 <vasok1991@gmail.com>
2023-04-08 00:39:17 +0300

    done task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
+// console.log(loginForm);
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    return alert("Заповніть поля");
+  }
+
+  const info = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(info);
+  event.currentTarget.reset();
+};
