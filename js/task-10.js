d2f8c74a1449f5a860fe649433eaec9595b3d84a
Mazurok001 <vasok1991@gmail.com>
2023-04-08 16:53:54 +0300

    done task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const textInput = document.querySelector('[type="number"]');
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = btnCreate.nextElementSibling;
+const newEl = document.querySelector('#boxes');
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener('click', () => { newEl.innerHTML = ""; });
+
+function createBoxes() {
+  const amount = textInput.value;
+  for (let i = 0; i < amount; i += 1) {
+    let createBoxSize = i * 10 + 30;
+
+    const markup = `<div style="background:${getRandomHexColor()};width:${createBoxSize}px;height:${createBoxSize}px"></div>`;
+
+    newEl.insertAdjacentHTML('afterbegin', markup);
+
+  };
+};
+
